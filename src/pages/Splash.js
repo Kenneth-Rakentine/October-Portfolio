90e9682e66b26cd8a2bbb2a4f5db40ece9ce76c2
Kenneth Rakentine <specialistmorgenj@gmail.com>
2023-10-12 06:59:02 -0400

    readme adjust

--- src/pages/Splash.js
+++ src/pages/Splash.js
@@ -5,7 +5,10 @@ import './Splash.css';
 function Splash() {
   return (
     <div className='splash-container'>
-        <img src="https://i.gifer.com/g2Hz.gif" alt='Splash Screen'/>
+        {/* <img src="https://i.gifer.com/g2Hz.gif" alt='Splash Screen machinery'/> */}
+        <img src="https://i.pinimg.com/originals/e2/ca/51/e2ca511664a6f9cf2c5d9f92328534ae.gif" alt='Splash Screen vhs'/>
+
+        
     </div>
   )
 }
